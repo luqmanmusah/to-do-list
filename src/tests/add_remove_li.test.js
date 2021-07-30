cfbd4af09e701d77fd1561a7ab6e5936c5b553af
alexander16108 <alexandermayowa05@gmail.com>
2021-07-30 12:15:14 +0100

    Adding test files to github

---
+++ src/tests/add_remove_li.test.js
@@ -0,0 +1,3 @@
+/* eslint-disable quotes */
+const { expect } = require("@jest/globals");
+const { displayTasks } = require("./tests-files/index");
--- tests/add_remove.test.js
+++
@@ -1,9 +0,0 @@
-import addRemove from '../src/add_remove';
-
-describe('its', () => {
-  it('should throw error if file is not removed', () => {
-    expect(() => {
-      addRemove.removeTask();
-    }).toThrow();
-  });
-});
