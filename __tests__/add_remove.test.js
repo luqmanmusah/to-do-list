73b378564003251c03ce312a3531c73c02794b0e
Luqman Musah <musahluqman@gmail.com>
2021-07-30 11:57:53 +0000

    Modify add_remove.test file

--- __tests__/add_remove.test.js
+++ __tests__/add_remove.test.js
@@ -1,8 +1,16 @@
-import { removeTask } from '../src/add_remove';
+// import { removeTask } from '../src/add_remove';
+import { addTask } from '../src/add_remove';
 
-describe('remove task from list', () => {
-  test('remove task', () => {
-    const str = data.replace('div', '');
+// describe('remove task from list', () => {
+//   test('remove task', () => {
+//     const str = data.replace('div', '');
 
+//   });
+// });
+
+
+describe('add and remove from list', () => {
+  test('add test', () => {
+    expect(addTask('First task')).toBe('First task');
   });
 });
