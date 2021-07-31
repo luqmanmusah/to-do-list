bd4ace3117e9adf64e6b01ced3f54f135d7e1b23
alexander16108 <alexandermayowa05@gmail.com>
2021-07-31 14:59:29 +0100

    Adding test files for drag and drop.

--- src/tests/sorting.test.js
+++ src/tests/sorting.test.js
@@ -1,22 +1,26 @@
-import { fireEvent, getByText } from "@testing-library/dom";
-import "@testing-library/jest-dom/extend-expect";
-import "regenerator-runtime/runtime";
+import { fireEvent, getByText } from '@testing-library/dom';
+
+import '@testing-library/jest-dom/extend-expect';
+
+import 'regenerator-runtime/runtime';
+
 import { drag, drop } from './tests-files/sorting';
+
 import './tests-files/index';
 
 displayTasks();
 
-describe("Functions for testing text inputs", () => {
-  const textInputs = global.document.getElementsByClassName("description");
+describe('Functions for testing text inputs', () => {
+  const textInputs = global.document.getElementsByClassName('description');
   Array.from(textInputs).forEach((textInput) => {
-    test("renders an Input Text element", () => {
+    test('renders an Input Text element', () => {
       expect(textInput).not.toBeNull();
       expect(textInput).toBeInTheDocument();
     });
-    test("focus In , inputs a text then focus Out  ", async () => {
+    test('focus In , inputs a text then focus Out  ', async () => {
       const oldText = textInput.value;
       fireEvent.focusIn(textInput);
-      textInput.value = "The test text";
+      textInput.value = 'The test text';
       fireEvent.input(textInput);
       fireEvent.focusOut(textInput);
       expect(textInput.value).not.toBe(oldText);
@@ -24,14 +28,14 @@ describe("Functions for testing text inputs", () => {
   });
 });
 
-describe("Functions for testing checkbox inputs", () => {
-  const checkInputs = global.document.getElementsByTagName("input");
+describe('Functions for testing checkbox inputs', () => {
+  const checkInputs = global.document.getElementsByTagName('input');
   Array.from(checkInputs).forEach((checkInput) => {
-    if (checkInput.type === "checkbox") {
-      test("renders all the input checkbox element", () => {
+    if (checkInput.type === 'checkbox') {
+      test('renders all the input checkbox element', () => {
         expect(checkInput).not.toBeNull();
       });
-      test("tests all input checkbox element ", async () => {
+      test('tests all input checkbox element ', async () => {
         const oldState = checkInput.checked;
         fireEvent.click(checkInput);
         expect(checkInput.checked).not.toBe(oldState);
